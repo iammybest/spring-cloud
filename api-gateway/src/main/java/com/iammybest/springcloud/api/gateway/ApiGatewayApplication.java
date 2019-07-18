a037da867788173346fdf5efdbc080d74da286fb
“qinghai.deng <qinghai.deng@timanetworks.com>
2019-07-18 11:15:59 +0800

    删除密码

---
+++ api-gateway/src/main/java/com/iammybest/springcloud/api/gateway/ApiGatewayApplication.java
@@ -0,0 +1,40 @@
+package com.iammybest.springcloud.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableWebFluxSecurity
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("service-internal", r -> r.path("/get")
+                        .uri("http://httpbin.org"))
+                .route("host_route", r -> r.host("*.myhost.org")
+                        .uri("http://httpbin.org"))
+                .build();
+    }
+
+    @Bean
+    SecurityWebFilterChain webFluxSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        http.authorizeExchange().pathMatchers("/service-internal").hasRole("ADMIN")
+                .anyExchange().permitAll().and().cors().and()
+                .httpBasic().and()
+                .csrf().disable();
+        return http.build();
+    }
+}
